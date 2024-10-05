9a78386e7aef5710f3fa3ab39b9039ce929aad93
EunJin Kim <eunjin9639@gmail.com>
2024-10-05 18:07:55 +0900

    🔀 merge toggle

--- src/stories/SToggle.stories.tsx
+++ src/stories/SToggle.stories.tsx
@@ -1,6 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
-import { useEffect, useState } from 'storybook/internal/preview-api';
 
 import SToggle, { type ToggleProps } from '../components/SToggle';
 
