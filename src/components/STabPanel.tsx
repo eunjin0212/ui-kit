a86c448d1909383f3823078dc9148a40f55276c9
EunJin Kim <eunjin9639@gmail.com>
2024-11-04 23:31:50 +0900

    🏷️ Edit type

--- src/components/STabPanel.tsx
+++ src/components/STabPanel.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import type { ReactNode } from 'react';
 
 export interface STabPanelProps {
 	value: string;
-	children: React.ReactNode;
+	children: ReactNode;
 	className?: string;
 }
 
